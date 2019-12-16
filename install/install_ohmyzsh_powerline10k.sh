64ac14ad05cc904a1e4903cda6add56ea2564430
janine <janine.cheng@gmail.com>
2019-12-15 18:15:49 -0800

    - removed unused install scripts

--- install/install_ohmyzsh_powerline10k.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-if [[ -z  ~/.oh-my-zsh/custom ]];then
-    mkdir -vp ~/.oh-my-zsh/custom
-fi
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
-
