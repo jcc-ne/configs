e2859bce3ec0e26f245557844032951b61244b31
janine <janine.cheng@gmail.com>
2019-12-15 18:12:39 -0800

    install_ohmyzsh includes submodule updates

--- install/install_ohmyzsh.sh
+++ install/install_ohmyzsh.sh
@@ -3,5 +3,7 @@
 # sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 
 git clone https://github.com/jcc-ne/ohmyzsh.git ~/.oh-my-zsh
+cd ~/.oh-my-zsh
+git checkout dev
+git update --init
 ./cp_dot_files.sh
-
