d441f9b5eb49e8a82cc1437edb86156d7488d5f3
Janine <janine.cheng@gmail.com>
2019-12-15 17:52:27 -0800

    install_ohmyzsh from jcc-ne repo

--- install/install_ohmyzsh.sh
+++ install/install_ohmyzsh.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+# sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+
+git clone https://github.com/jcc-ne/ohmyzsh.git ~/.oh-my-zsh
+./cp_dot_files.sh
+
--- install/install_ohmyzsh_powerline10k.sh
+++ install/install_ohmyzsh_powerline10k.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [[ -z  ~/.oh-my-zsh/custom ]];then
-    mkdir -v f~/.oh-my-zsh/custom
+    mkdir -vp ~/.oh-my-zsh/custom
 fi
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
 
