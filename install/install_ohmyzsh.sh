ff6c4b33403a237c3e92a144c9deeda2004c0fab
Janine <janine.cheng@gmail.com>
2019-12-19 13:24:24 -0800

    Merge dev: powerlevel10k, ohmyzsh upstream, nvim jedi deoplete
        - resize-pane key bindings in tmux
        - nerdcommenter add space
        - gitmerge tool update
        - add hostname on tmux
        - make add2fav theme relative path
        - removed unused install scripts
        - install_ohmyzsh includes submodule updates
        - move switch_theme plugin from .zshrc to .oh-my-zsh project dir/
        - use configs/install/install_ohmyzsh.sh script
        - install_ohmyzsh from jcc-ne repo
        - move fav_theme to custom/ohmyzsh_themes
        - nvim: install undotree
        - change bright green to darker green
        - add powerlevel10k
           * p9k and p10k files, add terse and lean versions
        - fix ultisnips dir issue
        - nvimrc: add fzf, deoplete, fix some key conflicts

--- install/install_ohmyzsh.sh
+++ install/install_ohmyzsh.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
-sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+# sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+
+git clone https://github.com/jcc-ne/ohmyzsh.git ~/.oh-my-zsh
+cd ~/.oh-my-zsh
+git checkout dev
+git update --init
+./cp_dot_files.sh
