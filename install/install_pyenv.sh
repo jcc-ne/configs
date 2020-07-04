5e47e89ea5a900cd6ce52ea2eebf45a95bb6ffbd
Janine <janine.cheng@gmail.com>
2020-07-04 13:03:57 -0700

    mbp: add csv plugin to nvim. change pyenv install to use brew

--- install/install_pyenv.sh
+++ install/install_pyenv.sh
@@ -1,15 +1,20 @@
 #!/bin/sh
 
+set -e
 
-if [ -z ~/.pyenv ]; then
-    git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+if [ ! -d ~/.pyenv ]; then
+    brew insall pyenv
+    # git clone https://github.com/pyenv/pyenv.git ~/.pyenv
 fi
 
 # echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zshrc
 # echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zshrc
 # echo 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >> ~/.zshrc
 
-if [ -z ~/.pyenv/plugins/pyenv-virtualenv ]; then
+if [ ! -d ~/.pyenv/plugins ]; then
+    mkdir ~/.pyenv/plugins
+fi
+if [ ! -d ~/.pyenv/plugins/pyenv-virtualenv ]; then
     git clone https://github.com/pyenv/pyenv-virtualenv.git $(pyenv root)/plugins/pyenv-virtualenv
 fi
 # echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.zshenv
@@ -17,11 +22,11 @@ fi
 # exec "$SHELL"
 
 echo  "installing..."
-pyenv install anaconda3-2020.02
+# pyenv install anaconda3-2020.02
 
-pyenv virtualenv anaconda3-2020.02 tf2
-pyenv activate tf2
+# pyenv virtualenv anaconda3-2020.02 tf2
+# pyenv activate tf2
 
-conda activate tf2
-
-conda install python=3.7 tensorflow-gpu
+# conda install python=3.7 tensorflow-gpu
+pyenv install 3.7.7
+pyenv virtualenv 3.7.7 py37
