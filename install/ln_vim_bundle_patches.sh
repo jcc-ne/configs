07196499c109b963a0119c0b07f67e6986d0c3cf
Janine <janine.cheng@gmail.com>
2022-08-31 09:09:48 -0700

    Add lua plugins install in ln_vim_bundle_patches.sh

--- install/ln_vim_bundle_patches.sh
+++ install/ln_vim_bundle_patches.sh
@@ -13,6 +13,10 @@ fi
 
 cd ~/.vim/bundle
 
-ln -s $DIR/../my_patch/myBundle .
+ln -s $DIR/../my_patch/myBundle || true
+
+mkdir -v -p ~/.config/nvim/lua
+cd ~/.config/nvim/lua
+ln -s $DIR/../dot_files/plugins.lua .
 
 cd $my_cwd
