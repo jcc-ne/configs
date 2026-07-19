5f9c65ab441ca5fa99206761fe9f62056b08d6d6
Janine <janine.cheng@gusto.com>
2026-07-18 21:18:41 -0700

    Fix MarkdownPreview

--- nvim/lua/neovim_standalone.lua
+++ nvim/lua/neovim_standalone.lua
@@ -160,7 +160,7 @@ return {
     {
         "iamcco/markdown-preview.nvim",
         cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-        build = "cd app && yarn install",
+        build = function() vim.fn["mkdp#util#install"]() end,
         init = function()
             vim.g.mkdp_filetypes = { "markdown" }
         end,
